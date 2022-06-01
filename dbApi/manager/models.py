e109cad94960329e118dcf404412f9fa9d107f82
hanwool95 <77750865+hanwool95@users.noreply.github.com>
2022-06-01 15:53:00 +0900

    Feat: Event Model 총체적인 Event를 통합하는 모델 작성. 통일된 key가 없어서 관계형은 어려울듯.

--- dbApi/manager/models.py
+++ dbApi/manager/models.py
@@ -62,3 +62,10 @@ class Px(models.Model):
     date = models.DateTimeField(blank=True, null=True)
     format_name = models.TextField(max_length=100, blank=True, null=True)
     format_content = models.TextField(max_length=1000, blank=True, null=True)
+
+
+class Event(models.Model):
+    number = models.TextField(max_length=10, blank=True, null=True)
+    date = models.DateTimeField(blank=True, null=True)
+    event_num = models.IntegerField(default=-1)
+    important = models.BooleanField()
